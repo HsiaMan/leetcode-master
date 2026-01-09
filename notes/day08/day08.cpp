cf678d367a20e62d02abcd8057bf4b8883fb30d5
HsiaMan <xiazhangxi@gmail.com>
2026-01-09 17:09:20 +0800

    add some note in leetcode

--- notes/day08/day08.cpp
+++ notes/day08/day08.cpp
@@ -1,3 +1,4 @@
+#include <algorithm>
 #include <cstddef>
 #include <iostream>
 #include <cstring>
@@ -11,6 +12,18 @@ int main(){
    for(const auto & elem: v1.at(0)){
       std::cout << "elem = " << elem << std::endl;
    }
+   std::string v2 = {'a','b','c','d','e','f','g','h','i','j','k'};
+   for (const auto & elem: v2) {
+      std::cout << elem << std::endl;
+   }
+   std::reverse(v2.begin(), v2.end());
+   std::cout << "v2 ="<< v2 << std::endl;
+   std::array<char, 11> v3 = {'a','b','c','d','e','f','g','h','i','j','\0'};
+   // std::reverse(v3.begin(), v3.end());
+   std::cout << "v3 = " << v3.data() << std::endl;
+   std::cout << "v3[1]= " << v3[1] << std::endl;
+
+   
    // 下面的写法是错误的
    // std::vector<char> s(8) = {'a','b','d','e','d','j','m','n'};
    std::vector<char> s2 = {'a','b','d','e','d','j','m','n'};
@@ -25,6 +38,7 @@ int main(){
    for (auto it= num1.begin(); it != num1.end() ;++it) {
       std::cout << *it << std::endl;
    }
+   
 
    // std::vector<char> s[8] = "abcdefg";
    return 0;
